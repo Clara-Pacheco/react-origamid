e0311cd277b8bea0be3440be1b3aa6c6f630c4fd
Clara <clarapachecodev@gmail.com>
2023-03-05 10:38:08 -0300

    Create component Products that filters an array of object and displays a jsx with the name, price and each color of each object

--- react_tests_origamid/src/main.jsx
+++ react_tests_origamid/src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import { Arrays } from './Arrays'
+import { Products } from './Products'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Arrays />
+    <Products />
   </React.StrictMode>,
 )
